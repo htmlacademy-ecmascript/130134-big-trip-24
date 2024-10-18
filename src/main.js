c03c86c4c29fa7e3f07d73b356346bf6f5949e70
Ivan Klimukhin <ivankmn@yandex.ru>
2024-10-19 00:39:15 +0300

    Создал модель фильтров, научил вьюху фильтра принимать текущее состояние фильтра и колбэк для переключения фильтра

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ import { render } from './framework/render.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
-import { generateFilter } from './mock/filter.js';
+import FilterModel from './model/filter-model.js';
+
+const filters = [
+  {
+    type: 'everything',
+    count: 0,
+  },
+];
 
 const filtersElement = document.querySelector('.trip-controls__filters');
 const mainContentElement = document.querySelector('.trip-events');
+const filterModel = new FilterModel();
 const pointsModel = new PointsModel();
 const boardPresenter = new BoardPresenter({
   boardContainer: mainContentElement,
   pointsModel,
 });
 
-const filters = generateFilter(pointsModel.points);
-
-render(new FilterView({ filters }), filtersElement);
+render(
+  new FilterView({
+    filters,
+    currentFilterType: 'everything',
+    onFilterTypeChange: () => {},
+  }),
+  filtersElement
+);
 
 boardPresenter.init();
